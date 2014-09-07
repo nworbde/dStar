163646dbcf3aa865727c0526798378b2b35787c4
Edward Brown <ebrown@pa.msu.edu>
2014-09-07 13:02:00 -0400

    test, P-rho relation
    table generation, interpolation appears to work. during generation, a
    ton of warnings about quantum effects generated.  would be good to have
    option to turn off warnings…

--- dStar_crust/test/src/test_crust.f
+++ dStar_crust/test/src/test_crust.f
@@ -5,40 +5,65 @@ program test_crust
     use nucchem_lib
     use superfluid_lib
     use dStar_eos_lib
-    use hz90
+    use dStar_crust_def
+    use dStar_crust_lib
     
-    integer, parameter :: Ntab = 500
+    integer, parameter :: Ntrial = 20
     integer :: i, ierr
-    real(dp), dimension(Ntab) :: lgP, lgrho
-    real(dp), dimension(HZ90_number,Ntab) :: Yion
-    integer, dimension(HZ90_number) :: charged_ids
-    integer :: ncharged
-    real(dp), dimension(Ntab) :: Xneut
-    type(composition_info_type), dimension(Ntab) :: ion_info
+    real(dp), dimension(Ntrial) :: lgP, lgRho, Xneut, lgNb, dlgRho, dlgNb
+!     real(dp), dimension(HZ90_number,Ntrial) :: Yion
+!     integer, dimension(HZ90_number) :: charged_ids
+!     integer :: ncharged
+    type(composition_info_type), dimension(Ntrial) :: ion_info
     integer :: eos_handle
+    real(dp) :: Tref
     
     call constants_init('',ierr)
+    call check_okay('constants_init',ierr)
+    
     call nucchem_init('../../data',ierr)
-	call sf_startup('../../data',ierr)
-	call sf_load_gaps('ns','gc','t72',ierr)
+    call check_okay('nucchem_init',ierr)
 	
-	call dStar_eos_startup('../../data')
-	eos_handle = alloc_dStar_eos_handle(ierr)
+    call sf_startup('../../data',ierr)
+    call check_okay('sf_startup',ierr)
+	
+    call sf_load_gaps('ns','gc','t72',ierr)
+    call check_okay('sf_load_gaps',ierr)
+	
+    call dStar_eos_startup('../../data')
+    call check_okay('dStar_eos_startup',ierr)
+	
+    eos_handle = alloc_dStar_eos_handle(ierr)
+    call check_okay('alloc_dStar_eos_handle',ierr)
     
-    if (ierr /= 0) stop
+    call dStar_crust_startup('../../data',ierr)
+    call check_okay('dStar_atm_startup',ierr)
     
-    lgP = [(26.5+5.0*real(i-1,dp)/real(Ntab-1,dp),i=1,Ntab)]
-    call do_make_crust(lgP,Yion,Xneut,charged_ids,ncharged,ion_info)
+    Tref = 1.0d8
+    call dStar_crust_load_table('hz90s',eos_handle, Tref,ierr)
+    call check_okay('dStar_crust_load_table',ierr)
     
-    call find_densities(eos_handle,lgP,lgRho,Yion,ncharged,charged_ids,ion_info)
-    write(*,'(21a9,/)') 'lg P','lg rho',HZ90_network(:)
-    do i = 1,Ntab
-        write(*,'(21f9.5)') lgP(i),lgRho(i),Xneut(i),Yion(1:ncharged,i)
-        if (mod(i,20) == 0) then
-            write(*,'(21a9,/)') 'lg P','lg rho',HZ90_network(:)
-        end if  
+    lgP = [(26.5+5.0*real(i-1,dp)/real(Ntrial-1,dp),i=1,Ntrial)]
+    
+    do i = 1,Ntrial
+        call dStar_crust_get_results(lgP(i),lgRho(i),dlgRho(i),lgNb(i),dlgNb(i),ierr)
+        call check_okay('dStar_crust_get_results',ierr)
+        write(*,'(5(f9.5,tr2))') lgP(i),lgRho(i),dlgRho(i),lgNb(i),dlgNb(i)
     end do
+    
+    call dStar_crust_shutdown
     call sf_shutdown
     call nucchem_shutdown
+
+contains
+	subroutine check_okay(msg,ierr)
+		use iso_fortran_env, only : error_unit
+		character(len=*), intent(in) :: msg
+		integer, intent(inout) :: ierr
+		if (ierr /= 0) then
+			write (error_unit,*) trim(msg)//': ierr = ',ierr
+			if (ierr < 0) stop
+		end if
+	end subroutine check_okay
 
 end program test_crust
