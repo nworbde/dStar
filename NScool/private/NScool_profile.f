d419f8136bfde10228c4454b213152985b3c97f8
Edward Brown <ebrown@pa.msu.edu>
2014-10-06 07:18:50 -0400

    more profile columns
    added area and gravity.

--- NScool/private/NScool_profile.f
+++ NScool/private/NScool_profile.f
@@ -4,32 +4,33 @@ module NScool_profile
    character(len=*), parameter :: scival = 'es15.6', intval = 'i15', fltval = 'f15.6'
    character(len=*), parameter :: filename_fmt = '(a,i4.4)'
    integer, parameter :: num_header_cols = 7, header_col_width=15
-   integer, parameter :: num_profile_cols = 17, profile_col_width=15
+   integer, parameter :: num_profile_cols = 19, profile_col_width=15
 
    character(len=*), parameter :: header_count_fmt = '(7'//intval//')'
    character(len=*), parameter :: header_title_fmt = '(7a15)'
    character(len=*), parameter :: header_val_fmt = '('//intval//','//scival//',2'//fltval//',3'//scival//')'
-   character(len=*), parameter :: profile_count_fmt = '(17'//intval//')'
-   character(len=*), parameter :: profile_title_fmt = '(17a15)'
+   character(len=*), parameter :: profile_count_fmt = '(19'//intval//')'
+   character(len=*), parameter :: profile_title_fmt = '(19a15)'
    character(len=*), parameter :: profile_val_fmt = &
-   & '('//intval//',2'//scival//',2'//fltval//',4'//scival//',4'//fltval//',4'//scival//')'
+   & '('//intval//',4'//scival//',2'//fltval//',4'//scival//',4'//fltval//',4'//scival//')'
    character(len=header_col_width), dimension(num_header_cols) :: header_cols = [character(len=header_col_width) ::  &
       & 'model', 'time', 'core mass', 'core radius', 'accretion rate', 'heating lum.', 'cooling lum.']
    character(len=profile_col_width),dimension(num_profile_cols) :: profile_cols = [character(len=profile_col_width) ::  &
-      & 'zone','mass','dm','eLambda','ePhi', &
+      & 'zone','mass','dm','area','gravity','eLambda','ePhi', &
       & 'temperature','luminosity','pressure','density', &
       & 'zbar','abar','Xn','Qimp', &
       & 'cp','eps_nu','eps_nuc','K' ]
    
    contains
    subroutine do_write_profile(id,ierr)
+       use constants_def
       use utils_lib, only: alloc_iounit, free_iounit
       integer, intent(in) :: id
       integer, intent(out) :: ierr
       type(NScool_info), pointer :: s
       character(len=256) :: filename
       integer :: iounit, iz, ih
-      real(dp) :: Lnu, Lnuc
+      real(dp) :: Lnu, Lnuc, grav
       character(len=16) ::profile_date, profile_time, profile_zone
 
       call get_NScool_info_ptr(id,s,ierr)
@@ -62,8 +63,9 @@ module NScool_profile
       write(iounit,profile_title_fmt) adjustr(profile_cols)
       write (iounit,*)
       do iz = 1, s% nz
-         write(iounit,profile_val_fmt) iz, s% m(iz), s% dm(iz), s% eLambda(iz), s% ePhi(iz),  &
-         &  s% T(iz), s% L(iz), s% P(iz), s% rho(iz),  &
+          grav = fourpi*GMsun*s% Mcore*s% eLambda(iz)/s% area(iz)
+         write(iounit,profile_val_fmt) iz, s% m(iz), s% dm(iz), s% area(iz), grav, s% eLambda(iz), s% ePhi(iz),  &
+         &  s% T(iz), s% L(iz), s% P_bar(iz), s% rho(iz),  &
          &  s% ionic(iz)% Z, s% ionic(iz)% A, s% Xneut(iz),  s% ionic(iz)% Q, &
          &  s% Cp(iz), s% enu(iz), s% enuc(iz), s% Kcond(iz)
       end do
