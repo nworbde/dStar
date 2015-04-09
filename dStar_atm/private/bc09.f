4ec2266fde2791139ed8320a4c8a50159d7f0e92
Edward Brown <ebrown@pa.msu.edu>
2015-04-09 17:39:26 -0400

    flesh out evaluation of photosphere condition

--- dStar_atm/private/bc09.f
+++ dStar_atm/private/bc09.f
@@ -1,4 +1,12 @@
 module bc09
+    
+    ! pointers for the photosphere routine
+    integer, parameter :: igrav = 1
+    integer, parameter :: itau = 2
+    integer, parameter :: iTeff = 3
+    integer, parameter :: number_photosphere_rpar = 3
+    integer, parameter :: ihandle = 1
+    integer, parameter :: number_photosphere_ipar = 1
 
 contains
     
@@ -27,7 +35,42 @@ contains
         integer, intent(in) :: eos_handle
         integer, intent(out) :: ierr
         
-        
     end subroutine find_photospheric_pressure    
+
+    real(dp) function photosphere(rho, dfdrho, lrpar, rpar, lipar, ipar, ierr)
+       ! returns with ierr = 0 if was able to evaluate f and df/dx at x
+       ! if df/dx not available, it is okay to set it to 0
+       use const_def, only: dp
+       integer, intent(in) :: lrpar, lipar
+       real(dp), intent(in) :: rho
+       real(dp), intent(out) :: dfdrho
+       integer, intent(inout), pointer :: ipar(:) ! (lipar)
+       real(dp), intent(inout), pointer :: rpar(:) ! (lrpar)
+       integer, intent(out) :: ierr
+       real(dp) :: gravity, tau_ph, Teff, P, kappa
+       integer :: eos_handle
+       type(composition_info_type) :: ion
+       type(conductivity_components) :: K
+       real(dp), dimension(num_dStar_eos_results) :: res
+       integer :: phase, ncharged
+       integer, dimension(:) :: charged_ids
+       
+       ierr = 0
+       gravity = rpar(igrav)
+       tau_ph = rpar(itau)
+       Teff = rpar(iTeff)
+       eos_handle = ipar(ihandle)
+       
+       call eval_crust_eos(eos_handle,rho,Teff,ionic,ncharged,charged_ids,Yion, &
+       		&   res,phase,use_default_nuclear_size)
+       
+       P = exp(res(i_lnP))
+
+       get_thermal_conductivity(rho,T,chi,Gamma,eta,ionic,K,which_components)
+       kappa = K(icond_kap)
+       
+       return P - 2.0*onethird*gravity/kappa
+    end function f
+
 
 end module bc09
