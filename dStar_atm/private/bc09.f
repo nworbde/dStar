bc554ce8e21f6954ab12a075065663121ec58a24
Edward Brown <ebrown@pa.msu.edu>
2015-04-11 05:34:13 -0400

    photosphere finder tested and working

--- dStar_atm/private/bc09.f
+++ dStar_atm/private/bc09.f
@@ -7,6 +7,7 @@ module bc09
     integer, parameter :: iPph = 4
     integer, parameter :: iKph = 5
     integer, parameter :: number_photosphere_rpar = 5
+    
     integer, parameter :: ihandle = 1
     integer, parameter :: ichem_id = 2
     integer, parameter :: number_photosphere_ipar = 2
@@ -45,31 +46,41 @@ contains
         deallocate(tabTb9,tabTeff6_4)
     end subroutine do_get_bc09_Teff
     
-    subroutine find_photospheric_pressure(Teff,grav,tau,Pphoto,eos_handle,ierr)
+    subroutine find_photospheric_pressure(Teff,grav,tau,rho_ph,P_ph,eos_handle,ierr)
+        use iso_fortran_env, only: error_unit
         use constants_def
         use nucchem_def, only : nuclide_not_found, nuclib
     	use nucchem_lib, only : get_nuclide_index
     	use num_lib
         integer, parameter :: default_maximum_iterations_photosphere = 20
-        real(dp), parameter :: default_tolerance_photosphere_rho = 1.0e-8_dp
-        real(dp), parameter :: default_tolerance_photosphere_condition = 1.0e-8_dp
+        real(dp), parameter :: default_tolerance_photosphere_lnrho = 1.0e-6_dp
+        real(dp), parameter :: default_tolerance_photosphere_condition = 1.0e-6_dp
         real(dp), intent(in) :: Teff,grav,tau
-        real(dp), intent(out) :: Pphoto
+        real(dp), intent(inout) :: rho_ph   ! on input set <= 0 to have routine generate guess; on 
+        !   output, it contains the value of the photospheric density
+        real(dp), intent(out) :: P_ph
         integer, intent(in) :: eos_handle
         integer, intent(out) :: ierr
-        real(dp) :: root_ph,rho,dfdrho
+        real(dp) :: lnrho_ph,lnrho
         integer, pointer :: ipar(:) => null() ! (lipar)
         real(dp), pointer :: rpar(:) => null()  ! (lrpar)
         integer :: i, lrpar, lipar
         real(dp) :: sigma_Th
         real(dp) :: fallback_Pphoto
-        real(dp) :: rho_guess, kappa_Th, A, Z, Pgas
-        real(dp) :: rho1, rho3, drho, y1, y3
-        real(dp) :: eps_rho, eps_ph
+        real(dp) :: lnrho_guess, kappa_Th, A, Z, Pgas
+        real(dp) :: lnrho1, lnrho3, dlnrho, ph1, ph3
+        real(dp) :: eps_lnrho, eps_ph
         integer :: maximum_iterations
         
         ierr = 0
         allocate(ipar(number_photosphere_ipar), rpar(number_photosphere_rpar))
+
+        lrpar = number_photosphere_rpar
+        lipar = number_photosphere_ipar
+        rpar(igrav) = grav
+        rpar(itau) = tau
+        rpar(iTeff) = Teff
+        ipar(ihandle) = eos_handle
 
         ! hardwire photosphere compostion to pure He
         ipar(ichem_id) = get_nuclide_index('he4')
@@ -77,55 +88,56 @@ contains
             ierr = nuclide_not_found
             return
         end if
-                
-        rpar(igrav) = grav
-        rpar(iTeff) = Teff
-        rpar(itau) = tau
-        ipar(ihandle) = eos_handle
-        lrpar = number_photosphere_rpar
-        lipar = number_photosphere_ipar
-        
-        ! use initial guess with ideal gas pressure and thomson scattering
-        A = nuclib% A(ipar(ichem_id))
-        Z = nuclib% Z(ipar(ichem_id))
-        sigma_Th = 8.0_dp*onethird*pi*(electroncharge**2/Melectron/clight2)**2
-        kappa_Th = sigma_Th* Z/A
-        fallback_Pphoto = 2.0_dp*onethird*arad*Teff**4   
-        Pgas = twothird*grav/kappa_th - onethird*arad*Teff**4
-        if (Pgas < 0.0_dp) then
-            ierr = negative_photosphere_gas_pressure
-            Pphoto = fallback_Pphoto
-            return
-        end if
-        rho_guess = Pgas*amu*A/(Z+1.0)/(boltzmann*Teff)
-        ! get brackets for root find
-        drho = 0.1_dp
-        maximum_iterations = 10
-        call look_for_brackets(rho_guess, drho, rho1, rho3, photosphere, y1, y3, maximum_iterations,  &
-            & lrpar, rpar, lipar, ipar, ierr)
-        if (ierr /= 0) then
-            print *,'unable to bracket root: ierr = ', ierr
-            Pphoto = fallback_Pphoto
-            return
-        end if
-        
+
      	! set iteration count, tolerances
         maximum_iterations = default_maximum_iterations_photosphere
-        eps_rho = default_tolerance_photosphere_rho
+        eps_lnrho = default_tolerance_photosphere_lnrho
         eps_ph = default_tolerance_photosphere_condition
+        fallback_Pphoto = 2.0_dp*onethird*arad*Teff**4   
+        
+        ! use initial guess with ideal gas pressure and thomson scattering
+        if (rho_ph < 0.0_dp) then
+            A = nuclib% A(ipar(ichem_id))
+            Z = nuclib% Z(ipar(ichem_id))
+            sigma_Th = 8.0_dp*onethird*pi*(electroncharge**2/Melectron/clight2)**2
+            kappa_Th = sigma_Th/amu* Z/A
+            Pgas = tau*grav/kappa_th - onethird*arad*Teff**4
+            if (Pgas < 0.0_dp) then
+                ierr = negative_photosphere_gas_pressure
+                P_ph = fallback_Pphoto
+                return
+            end if
+            lnrho_guess = log(Pgas*amu*A/(Z+1.0)/(boltzmann*Teff))
+        else
+            lnrho_guess = log(rho_ph)
+        end if
 
-		root_ph = safe_root_with_initial_guess(photosphere,rho_guess,rho1,rho3,y1,y3, &
-            &   maximum_iterations,eps_rho,eps_ph,lrpar,rpar,lipar,ipar,ierr)
-
+        ! get brackets for root find
+        dlnrho = 0.1_dp
+        call look_for_brackets(lnrho_guess, dlnrho, lnrho1, lnrho3, photosphere, ph1, ph3, &
+             & maximum_iterations, lrpar, rpar, lipar, ipar, ierr)
         if (ierr /= 0) then
-            write(*,*) 'unable to converge', rho_guess, rho1, rho3, y1, y3
+            write(error_unit,*) 'unable to bracket root: ierr = ', ierr
+            P_ph = fallback_Pphoto
+            rho_ph = exp(lnrho_guess)
             return
         end if
-        Pphoto = rpar(iPph)
+        
+		lnrho_ph = safe_root_with_initial_guess(photosphere,lnrho_guess,lnrho1,lnrho3,ph1,ph3, &
+            &   maximum_iterations,eps_lnrho,eps_ph,lrpar,rpar,lipar,ipar,ierr)
+
+        if (ierr /= 0) then
+            write(error_unit,*) 'unable to converge on photospheric density: ',lnrho_guess,lnrho_ph
+            P_ph = fallback_Pphoto
+            rho_ph = exp(lnrho_guess)
+            return
+        end if
+        P_ph = rpar(iPph)
+        rho_ph = exp(lnrho_ph)
         deallocate(ipar, rpar)
     end subroutine find_photospheric_pressure    
 
-    real(dp) function photosphere(rho, dfdrho, lrpar, rpar, lipar, ipar, ierr)
+    real(dp) function photosphere(lnrho, dfdlnrho, lrpar, rpar, lipar, ipar, ierr)
        ! returns with ierr = 0 if was able to evaluate f and df/dx at x
        ! if df/dx not available, it is okay to set it to 0
        use constants_def
@@ -135,12 +147,12 @@ contains
        use conductivity_lib
        
        integer, intent(in) :: lrpar, lipar
-       real(dp), intent(in) :: rho
-       real(dp), intent(out) :: dfdrho
+       real(dp), intent(in) :: lnrho
+       real(dp), intent(out) :: dfdlnrho
        integer, intent(inout), pointer :: ipar(:) ! (lipar)
        real(dp), intent(inout), pointer :: rpar(:) ! (lrpar)
        integer, intent(out) :: ierr
-       real(dp) :: gravity, tau_ph, Teff, P, kappa, Gamma, eta, Xsum, chi
+       real(dp) :: rho, gravity, tau_ph, Teff, P, kappa, Gamma, eta, Xsum, chi
        integer :: eos_handle
        type(composition_info_type) :: ionic
        type(conductivity_components) :: K
@@ -151,12 +163,7 @@ contains
 	   
        ierr = 0
        
-       ! check inputs
-       if (rho < 0.0) then
-           ierr = negative_photosphere_density
-           return
-       end if
-       
+       rho = exp(lnrho)
        gravity = rpar(igrav)
        tau_ph = rpar(itau)
        Teff = rpar(iTeff)
@@ -179,7 +186,7 @@ contains
            & Gamma,eta,ionic,K,which_components=cond_use_only_kap)
        kappa = 4.0*onethird*arad*clight*Teff**3/rho/K% kap
        rpar(iKph) = kappa
-	   dfdrho = 0.0
+	   dfdlnrho = 0.0
        
        photosphere = P - 2.0*onethird*gravity/kappa
     end function photosphere
--- dStar_atm/test/src/test_bc09.f
+++ dStar_atm/test/src/test_bc09.f
@@ -6,19 +6,31 @@ program test_bc09
     use conductivity_lib
     use bc09
 
-    integer :: eos_handle, ierr
-    real(dp) :: grav, Teff, tau, Pphoto
+    integer :: eos_handle, ierr, i
+    real(dp) :: grav, lnTeff, Teff, tau, rho_ph, P_ph
     
     call constants_init('',ierr)
     call nucchem_init('../../data',ierr)
     call dStar_eos_startup('../../data')
     eos_handle = alloc_dStar_eos_handle(ierr)
 
-    Teff = 5.0e6_dp
     grav = 2.43e14_dp
     tau = 2.0*onethird
-    call find_photospheric_pressure(Teff,grav,tau,Pphoto,eos_handle,ierr)
-
+    rho_ph = -1.0_dp
+    
+    do i = 20,1,-1
+        lnTeff = log10(5.0e5) + (i-1)/19.0
+        Teff = 10.0_dp**lnTeff
+        call find_photospheric_pressure(Teff,grav,tau,rho_ph,P_ph,eos_handle,ierr)
+        if (ierr /= 0) then
+            print *,'error: ierr = ',ierr
+            rho_ph = -1.0
+            cycle
+        end if
+        write (*,'(3(a,es11.4))') 'rho_ph = ',rho_ph,'    P_ph   = ',P_ph, &
+            & '    ln(rho_ph) = ',log(rho_ph)
+    end do
+    
     call dStar_eos_shutdown
     call nucchem_shutdown
 
