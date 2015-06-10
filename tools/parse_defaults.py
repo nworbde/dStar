82efb8c8124c04d7eede721cbfd04937b65b55a3
Edward Brown <ebrown@pa.msu.edu>
2015-06-10 17:02:12 -0400

    class for namelist control

--- tools/parse_defaults.py
+++ tools/parse_defaults.py
@@ -1,19 +1,45 @@
 from __future__ import print_function, division
 from os import environ
 
-default_file = environ['DSTAR_DIR']+'/NScool/defaults/controls.defaults'
+class dStarRun:
+    
+    def __init__(self,dStar_dir=''):
+        self._controls = {}
+        if dStar_dir == '':
+            try:
+                dStar_dir = environ['DSTAR_DIR']
+            except KeyError:
+                print('supply the root directory of dStar, either as an argument or through the environment variable DSTAR_DIR')
+                return
 
-defaults = {}
+        _defaults_file = dStar_dir+'/NScool/defaults/controls.defaults'
 
-with open(default_file,'r') as f:
-    for line in f:
-        # strip comments and whitespace: everthing to right of ! is ignored
-        toks = line.split('!')[0].split()
-        if len(toks) == 0: continue
-        # now smash toks back together and split on '='
-        toks = ''.join(toks).split('=')
-        k, v = toks[0],toks[1]
-        defaults[k] = v
+        with open(_defaults_file,'r') as f:
+            for line in f:
+                # strip comments and whitespace: everthing to right of ! is ignored
+                toks = line.split('!')[0].split()
+                if len(toks) == 0: continue
+                # now smash toks back together and split on '='
+                toks = ''.join(toks).split('=')
+                k, v = toks[0],toks[1]
+                self._controls[k] = v
 
-for k,v in defaults.iteritems():
-    print(k+' = '+v)
+    def print_namelist(self):
+        print('&controls')
+        for control,value in self._controls.iteritems():
+            print('    {0} = {1}'.format(control,value))
+        print('/')
+
+    def write_namelist_to_file(self,filename):
+        with open(filename,'w') as f:
+            f.write('&controls\n')
+            for control,value in self._controls.iteritems():
+                f.write('    {0} = {1}\n'.format(control,value))
+            f.write('/\n')
+    
+    def set_control(self,control,value):
+        if control in self._controls:
+            self._controls[control] = value
+        else:
+            print('bad control value {0}'.format(control))
+            raise KeyError(control)
