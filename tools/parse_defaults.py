596a7963ef1bbb04d3ebcc8036c9441873767698
Edward Brown <ebrown@pa.msu.edu>
2015-06-11 08:50:32 -0400

    improve functionality of parse_defaults
    explicit handing of arrays; nameless print simplified.

--- tools/parse_defaults.py
+++ tools/parse_defaults.py
@@ -1,10 +1,14 @@
 from __future__ import print_function, division
 from os import environ
+from sys import stdout
 
 class dStarRun:
     
+    _num_arr_controls = 32
+    
     def __init__(self,dStar_dir=''):
         self._controls = {}
+        self._defaults = {}
         if dStar_dir == '':
             try:
                 dStar_dir = environ['DSTAR_DIR']
@@ -21,24 +25,32 @@ class dStarRun:
                 if len(toks) == 0: continue
                 # now smash toks back together and split on '='
                 toks = ''.join(toks).split('=')
-                k, v = toks[0],toks[1]
-                self._controls[k] = v
+                # extra care needed for arrays
+                if '(:)' not in toks[0]:
+                    k, v = toks[0],toks[1]
+                    self._defaults[k] = v
+                else:
+                    v = toks[1]
+                    for i in range(1,dStarRun._num_arr_controls+1):
+                        k = toks[0].replace('(:)','('+str(i)+')')
+                        self._defaults[k] = v
 
-    def print_namelist(self):
-        print('&controls')
+    def print_namelist(self,filename=None):
+        if filename is None:
+            f = stdout
+        else:
+            try:
+                f = open(filename,'w')
+            except:
+                print('unable to open '+filename)
+                f = stdout
+        f.write('&controls\n')
         for control,value in self._controls.iteritems():
-            print('    {0} = {1}'.format(control,value))
-        print('/')
+            f.write('    {0} = {1}\n'.format(control,value))
+        f.write('/\n')
 
-    def write_namelist_to_file(self,filename):
-        with open(filename,'w') as f:
-            f.write('&controls\n')
-            for control,value in self._controls.iteritems():
-                f.write('    {0} = {1}\n'.format(control,value))
-            f.write('/\n')
-    
     def set_control(self,control,value):
-        if control in self._controls:
+        if control in self._defaults:
             self._controls[control] = value
         else:
             print('bad control value {0}'.format(control))
