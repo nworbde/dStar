a7189786a2896babab0380d00d76100c0ab2808c
Edward Brown <ebrown@pa.msu.edu>
2016-01-24 21:30:26 -0500

    fleshed out module for core

--- MRcurve/public/dStar_core_def.f
+++ MRcurve/public/dStar_core_def.f
@@ -2,8 +2,8 @@ module dStar_core_def
     use constants_def
 	real(dp), parameter :: rho_saturation = 0.16	! fm
     integer, parameter :: core_default_number_table_points = 2048
-    real(dp), parameter :: core_default_lgRhomin = 0.25*rho_saturation
-    real(dp), parameter :: core_default_lgRhomax = 8.0*rho_saturation
+    real(dp), parameter :: core_default_Rhomin = 0.25*rho_saturation
+    real(dp), parameter :: core_default_Rhomax = 8.0*rho_saturation
     
     type core_table_type
         logical :: is_loaded
@@ -19,6 +19,7 @@ module dStar_core_def
     type(core_table_type), target :: core_table
     
 	integer, parameter :: skyrme_matter = 1, skyrme_neutron = 2, skyrme_sym = 3
+
 	type skyrme
 		integer :: model
 		real(dp) :: gamma
@@ -26,6 +27,7 @@ module dStar_core_def
 	end type skyrme
 
 	type(skyrme), target :: skyrme_eos(3)
+	
 	character(len=256), save :: core_datadir
 	logical :: dStar_core_is_initialized = .FALSE.
 	
