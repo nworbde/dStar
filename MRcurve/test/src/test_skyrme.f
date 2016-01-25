6f1cf5bbb1e442d95f20b095288cdf87f245fc21
Edward Brown <ebrown@pa.msu.edu>
2016-01-24 21:51:17 -0500

    test of tables.  looks good.

--- MRcurve/test/src/test_skyrme.f
+++ MRcurve/test/src/test_skyrme.f
@@ -1,32 +1,43 @@
 program test_skyrme
-	use constants_def, only: dp
+	use constants_def
 	use constants_lib
 	use dStar_core_def
 	use dStar_core_lib
-	use load_skyrme_parameters
-	use brown_skyrme
 
-	integer, parameter :: Ntab = 20
 	integer :: i, ierr
-	real(dp), dimension(Ntab) :: rho, x, eps, P, muhat, mue, cs2
+	real(dp) :: lgP,lgRho,lgEps,Xprot,dlgRho,dlgEps,dXprot
 	
 	call constants_init('',ierr)
 	call check_okay('constants_init',ierr)
 	call dstar_core_startup('../../data',ierr)
 	call check_okay('dstar_core_startup',ierr)
-	call load_skyrme_table('s7d',ierr)
-	call check_okay('load_skyrme_table',ierr)
-	
-	rho = [(0.5 + 6.0*real(i-1,dp)/real(Ntab-1,dp), i=1,Ntab)]
-	rho = rho*rho_saturation
-	call find_beta_eq(rho,x,eps,P,muhat,mue,cs2,ierr)
-	call check_okay('find_beta_eq',ierr)
 
-	do i = 1,Ntab
-		write(*,*) rho(i), x(i), eps(i), P(i), cs2(i), muhat(i)-mue(i)
-	end do
+	write (*,'(7(a9,tr2),/,7("=========",tr2))') 'lgP','lgRho','dlgRho', &
+		& 'lgEps','dlgEps','Xprot','dXprot'
+
+	call do_one('s7d')
+	call do_one('s7a')
+	call do_one('s7r')
+	call dStar_core_shutdown
 contains
-			
+	subroutine do_one(model)
+		character(len=*), intent(in) :: model
+		integer :: i
+		
+        write (*,'(/,a)') trim(model)
+        
+    	call dStar_core_load_table(trim(model),ierr)
+    	call check_okay('dStar_core_load_table',ierr)
+        
+    	do i = 1, 20
+    		lgP = (-0.5_dp + 3.5_dp*real(i-1.0,dp)/19.0_dp)
+    		call dStar_core_get_results(lgP,lgRho,dlgRho,lgEps,dlgEps,Xprot,dXprot,ierr)
+    		call check_okay('dStar_core_get_results',ierr)
+    		write (*,'(7(f9.5,tr2))') lgP,lgRho,dlgRho,lgEps,dlgEps,Xprot,dXprot
+    	end do
+    	call dStar_core_free_table
+	end subroutine do_one
+	
 	subroutine check_okay(msg,ierr)
 		use iso_fortran_env, only : error_unit
 		character(len=*), intent(in) :: msg
