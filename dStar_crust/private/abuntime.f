a94f9cf6ccd5a7bbda9f5d94b9fb2e407816d77e
Edward Brown <ebrown@pa.msu.edu>
2016-07-18 08:59:44 -0700

    added routine to make crust
    weird problems — NaNs and non-monotonic pressure.  Could be ion EOS?

--- dStar_crust/private/abuntime.f
+++ dStar_crust/private/abuntime.f
@@ -3,7 +3,80 @@ module abuntime
 
 contains
     
-    subroutine read_abuntime(abuntime_filename, nz, nion, rho, isos, Yion, ierr)
+    subroutine make_abuntime_crust(eos_handle, rho, T, isos, abunds, lgP,  &
+    &   Yion, Xneut, charged_ids, ncharged, ion_info)
+        ! call this after read_abuntime
+        use const_def
+        use nucchem_def
+        use nucchem_lib
+    	use superfluid_def
+    	use superfluid_lib
+    	use dStar_eos_lib
+        
+        integer, intent(in) :: eos_handle
+        real(dp), dimension(:), intent(in) :: rho
+        real(dp), dimension(:), intent(in) :: T
+        character(len=iso_name_length), dimension(:), intent(in) :: isos
+        real(dp), dimension(:,:), intent(in) :: abunds
+        real(dp), dimension(:), intent(out) :: lgP
+        real(dp), dimension(:,:), intent(out) :: Yion
+        real(dp), dimension(:), intent(out) :: Xneut
+        integer, intent(out), dimension(:) :: charged_ids
+        integer, intent(out) :: ncharged
+        type(composition_info_type), dimension(:), intent(out) :: ion_info
+        !
+        integer, dimension(max_nnuclib) :: network_indcs
+        integer, dimension(:), allocatable :: indcs
+        real(dp) :: Xsum
+        integer :: nz, nion
+        integer :: i, n_indx
+		real(dp) :: k,Tcs(max_number_sf_types),chi,kFn,kFp
+		integer :: phase
+    	type(crust_eos_component), dimension(num_crust_eos_components) :: &
+    	& eos_components
+    	real(dp), dimension(num_dStar_eos_results) :: res
+        
+        nz = size(rho)
+        nion = size(isos)
+        
+        allocate(indcs(nion))
+        
+        ! set the network pointers
+        indcs = [(get_nuclide_index(adjustl(isos(i))),i=1,nion)]
+        
+        do i = 1, nion
+            if (indcs(i) == -1) then
+                print *,isos(i),': index not found'
+            end if
+        end do
+
+        ! and the reverse lookup
+        network_indcs = 0
+        network_indcs(indcs) = [(i,i=1,nion)]
+        n_indx = network_indcs(get_nuclide_index('n'))
+        
+        do i = 1, nz
+            call compute_composition_moments(nion,indcs,abunds(:,i), &
+            &   ion_info(i),Xsum,ncharged,charged_ids,Yion(:,i), &
+            &   exclude_neutrons=.TRUE., abunds_are_mass_fractions=.FALSE.)
+            
+			chi = nuclear_volume_fraction(rho(i),ion_info(i), &
+			&   default_nuclear_radius)
+			kFn = neutron_wavenumber(rho(i),ion_info(i),chi)
+			kFp = 0.0_dp
+			call sf_get_results(kFp,kFn,Tcs)
+			call eval_crust_eos(eos_handle,rho(i),T(i),ion_info(i), &
+			&   ncharged,charged_ids, Yion(:,i), Tcs, res, phase, chi, &
+			&   eos_components)
+            
+            lgP(i) = res(i_lnP)/ln10
+        end do
+        Xneut = abunds(n_indx,:)
+        
+    end subroutine make_abuntime_crust
+    
+    subroutine read_abuntime(abuntime_filename, nz, nion, rho, T,  &
+    &   isos, Yion, ierr)
         
         use iso_fortran_env, only: iostat_end, error_unit
         use const_def
@@ -13,6 +86,7 @@ contains
         integer, intent(out) :: nz  ! number of zones
         integer, intent(out) :: nion    ! number of species
         real(dp), intent(out), dimension(:), allocatable :: rho  ! nz
+        real(dp), intent(out), dimension(:), allocatable :: T    ! nz
         character(len=iso_name_length), dimension(:), allocatable :: isos ! nion
         real(dp), intent(out), dimension(:,:), allocatable :: Yion   ! (nion,nz)
         integer, intent(out) :: ierr
@@ -20,7 +94,7 @@ contains
         integer, parameter :: default_chunk_size = 4096
         integer :: unitno, ios
         integer :: k
-        real(dp) :: time,temp,EFe,EFn
+        real(dp) :: time,temp,EFe,EFn,last_rho
         
         open(newunit=unitno,file=trim(abuntime_filename), &
         &   action='read',status='old', iostat=ierr)
@@ -32,10 +106,11 @@ contains
         allocate(isos(nion))
         
         allocate(rho(default_chunk_size),Yion(nion,default_chunk_size), &
-            & stat=ierr)
+            & T(default_chunk_size), stat=ierr)
         if (failure('allocating rho,Yion',ierr)) return
 
         nz = 1
+        last_rho = 0.0
         do
             ! size check
             if (nz > size(rho)) then
@@ -43,7 +118,7 @@ contains
                 if (failure('reallocating abuntime arrays',ierr)) return
             end if
             read(unitno,'(1x,1e18.10,2e11.3,2X,2E18.10)',iostat=ios) &
-                 & time,temp,rho(nz),EFe,EFn
+                 & time,T(nz),rho(nz),EFe,EFn
             if (ios == iostat_end) then
                 nz = nz-1
                 exit
@@ -51,9 +126,14 @@ contains
                 print *,'abnormal return: ierr = ', ierr
                 return
             end if
+            ! only accept if rho has incremented
             read(unitno,'(1x,5(a5,1pe10.3))') (isos(k),Yion(k,nz),k=1,nion)
             read(unitno,*)
-            nz = nz+1
+            ! only increment nz if rho has increased
+            if (rho(nz) > last_rho) then
+                last_rho = rho(nz)
+                nz = nz+1
+            end if
             if (modulo(nz,100) == 0)  &
             & write (error_unit,'(a)',advance='no') '.'
         end do
@@ -67,21 +147,24 @@ contains
             integer, intent(in) :: newsize
             integer, intent(out) :: ierr
             integer :: currentsize, oldsize
-            real(dp), dimension(:), allocatable :: tmp_rho
+            real(dp), dimension(:), allocatable :: tmp_rho, tmp_T
             real(dp), dimension(:,:), allocatable :: tmp_Yion
             
             oldsize = size(rho)
-            allocate(tmp_rho(newsize),tmp_Yion(nion,newsize),stat=ierr)
+            allocate(tmp_rho(newsize),tmp_T(newsize),tmp_Yion(nion,newsize), &
+            &   stat=ierr)
             if (ierr /= 0) return
             currentsize = min(oldsize,newsize)
             tmp_rho(1:currentsize) = rho(1:currentsize)
+            tmp_T(1:currentsize) = T(1:currentsize)
             tmp_Yion(:,1:currentsize) = Yion(:,1:currentsize)
-            deallocate(rho,Yion)
-            allocate(rho(newsize),Yion(nion,newsize),stat=ierr)
+            deallocate(rho,T,Yion)
+            allocate(rho(newsize),T(newsize),Yion(nion,newsize),stat=ierr)
             if (ierr /= 0) return
             rho(1:newsize) = tmp_rho(1:newsize)
+            T(1:newsize) = tmp_T(1:newsize)
             Yion(:,1:newsize) = tmp_Yion(:,1:newsize)
-            deallocate(tmp_rho,tmp_Yion)
+            deallocate(tmp_rho,tmp_T,tmp_Yion)
         end subroutine realloc_abuntime_arrays
     end subroutine read_abuntime
 
@@ -96,85 +179,5 @@ contains
             write(error_unit,*) trim(msg),': ierr = ',ierr
         end if
     end function failure
-
-!     subroutine do_make_crust(lgP, Yion, Xneut, charged_ids, ncharged, ion_info)
-!         use const_def
-!         use nucchem_def
-!         use nucchem_lib
-!         real(dp), intent(in), dimension(:) :: lgP
-!         real(dp), intent(out), dimension(:,:) :: Yion   ! (HZ90_number, size(lgP))
-!         real(dp), intent(out), dimension(:) :: Xneut    ! (size(lgP))
-!         integer, intent(out), dimension(HZ90_number) :: charged_ids
-!         integer, intent(out) :: ncharged
-!         type(composition_info_type), dimension(:), intent(out) :: ion_info   ! (size(lgP))
-!         integer, dimension(max_nnuclib) :: network_indcs
-!
-!         real(dp), allocatable, dimension(:,:) :: X
-!         integer :: Ntab, i, indx, n_indx, indx1, indx2
-!         integer, dimension(HZ90_number) :: indcs
-!
-!         real(dp), dimension(number_layers) :: lg_Pt
-!         real(dp) :: lgP1, lgP2, width, Xsum
-!
-!         Ntab = size(lgP)
-!         allocate(X(HZ90_number,Ntab))
-!
-!         lg_Pt = log10(transition_pressures)
-!
-!         ! set the network pointers
-!         indcs = [(get_nuclide_index(HZ90_network(i)),i=1,HZ90_number)]
-!
-!         ! and the reverse lookup
-!         network_indcs = 0
-!         network_indcs(indcs) = [(i,i=1,HZ90_number)]
-!
-!         ! for each layer set composiiton...we'll smooth the transitions in the next step
-!         X = 0.0
-!         ! first layer
-!         indx = network_indcs(get_nuclide_index(ion_composition(1)))
-!         n_indx = network_indcs(get_nuclide_index('n'))
-!         where(lgP <= lg_Pt(1))
-!             X(n_indx,:) = Xn(1)
-!             X(indx,:) = 1.0-Xn(1)
-!         end where
-!
-!         do i = 2, number_layers
-!             indx = network_indcs(get_nuclide_index(ion_composition(i)))
-!             where(lgP > lg_Pt(i-1) .and. lgP <= lg_Pt(i))
-!                 X(n_indx,:) = Xn(i)
-!                 X(indx,:) = 1.0-Xn(i)
-!             end where
-!         end do
-!
-!         indx = network_indcs(get_nuclide_index(ion_composition(number_layers+1)))
-!         where (lgP > lg_Pt(number_layers))
-!             X(n_indx,:) = Xn(number_layers+1)
-!             X(indx,:) = 1.0-Xn(number_layers+1)
-!         end where
-!
-!         ! now smooth the transitions
-!         do i = 1, number_layers
-!             lgP1 = lg_Pt(i) - transition_width
-!             lgP2 = lg_Pt(i) + transition_width
-!             width = 2.0*transition_width
-!             indx1 = network_indcs(get_nuclide_index(ion_composition(i)))
-!             indx2 = network_indcs(get_nuclide_index(ion_composition(i+1)))
-!             where(lgP >= lgP1 .and. lgP <= lgP2)
-!                 X(n_indx,:) = (Xn(i)-Xn(i+1))*cos(0.5*pi*(lgP-lgP1)/width) + Xn(i+1)
-!                 X(indx1,:) = (1.0-X(n_indx,:))*cos(0.5*pi*(lgP-lgP1)/width)
-!                 X(indx2,:) = (1.0-X(n_indx,:))*(1.0 - cos(0.5*pi*(lgP-lgP1)/width))
-!             end where
-!         end do
-!
-!         ! loop over and compute composition moments
-!         do i = 1, Ntab
-!             call compute_composition_moments(HZ90_number,indcs,X(:,i), &
-!             &   ion_info(i),Xsum,ncharged,charged_ids,Yion(:,i),exclude_neutrons=.TRUE., &
-!             &   abunds_are_mass_fractions=.TRUE.)
-!         end do
-!         Xneut = X(n_indx,:)
-!
-!         deallocate(X)
-!     end subroutine do_make_crust
     
 end module abuntime
--- dStar_crust/test/src/test_abuntime.f
+++ dStar_crust/test/src/test_abuntime.f
@@ -3,26 +3,56 @@ program test_abuntime
     use constants_lib
     use nucchem_def
     use nucchem_lib
+	use superfluid_def
+	use superfluid_lib
+	use dStar_eos_lib
     use abuntime
 
     character(len=*), parameter ::  &
-        & abuntime_filename ='../data/abuntime_lx2_5.txt'
+        & abuntime_filename ='../data/abuntime_lx2_5.data'
 
     integer :: nz, nion
-    real(dp), dimension(:), allocatable :: rho
-    real(dp), dimension(:,:), allocatable :: Yion
+    real(dp), dimension(:), allocatable :: rho, T
+    real(dp), dimension(:,:), allocatable :: Yion, abunds
     character(len=iso_name_length), dimension(:), allocatable :: isos
     integer :: k,ierr
+    real(dp), dimension(:), allocatable :: lgP, Xneut
+    integer, dimension(:), allocatable :: charged_ids
+    integer :: ncharged
+    type(composition_info_type), dimension(:), allocatable :: ion_info
+    integer :: eos_handle,sf_handle
     
     ierr = 0
     call constants_init('',ierr)
     call check_okay('constants_init',ierr)
+    call nucchem_init('../../data/',ierr)
+    call check_okay('nucchem_init',ierr)
+	call sf_startup('../../data',ierr)
+    call check_okay('sf_startup',ierr)
+	call sf_load_gaps('ns','gc','t72',ierr)
+    call check_okay('load gaps',ierr)
+	
+	call dStar_eos_startup('../../data')
+	eos_handle = alloc_dStar_eos_handle(ierr)
+    call check_okay('allocate eos',ierr)
     
     print *,'reading abuntime'
-    call read_abuntime(abuntime_filename, nz, nion, rho, isos, Yion, ierr)
+    call read_abuntime(abuntime_filename, nz, nion, rho, T, isos, abunds, ierr)
     print *,'done'
-    print *,rho(nz),(isos(k),Yion(k,nz),k=1,15)
+    print *,rho(nz),(isos(k),abunds(k,nz),k=1,15)
+    
+    allocate(Yion(nion,nz),Xneut(nz),charged_ids(nion),ion_info(nz), lgP(nz))
+    
+    call make_abuntime_crust(eos_handle, rho, T, isos, abunds, lgP, Yion, Xneut, &
+    & charged_ids, ncharged, ion_info)
 
+    do k = 1,nz
+        write(*,'(2es11.4,3f7.2,es11.4,f7.3)') lgP(k),rho(k),ion_info(k)% Z, ion_info(k)% A, ion_info(k)% Q, Xneut(k), sum(abunds(:,k))
+    end do
+    
+	call sf_shutdown
+	call nucchem_shutdown
+    
 contains
 	subroutine check_okay(msg,ierr)
 		use iso_fortran_env, only : error_unit
