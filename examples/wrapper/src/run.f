319758d8acdf7324e3c8ae3471473a7a744d685e
Edward Brown <ebrown@pa.msu.edu>
2017-02-08 11:19:22 -0500

    more options for wrapper example
    added core mass and radius

--- examples/wrapper/src/run.f
+++ examples/wrapper/src/run.f
@@ -7,12 +7,14 @@ program run_dStar
     
     character(len=*), parameter :: default_dStar_dir = '../../dStar'
     character(len=*), parameter :: default_inlist_file = 'inlist'
-    character(len=*), parameter :: default_Q_heating_shallow = '0.0'
-    character(len=64) :: my_dStar_dir
-    character(len=64) :: inlist
-    character(len=64) :: Q_heating_shallow_arg
-    real(dp) :: Q_heating_shallow
-    real(dp) :: eV_to_MK
+    character(len=*), parameter :: default_Q_heating_shallow = '0.0' ! MeV
+    character(len=*), parameter :: default_core_mass = '1.6' ! Msun
+    character(len=*), parameter :: default_core_radius = '11.0' ! km
+    
+    character(len=64) :: my_dStar_dir, inlist, Q_heating_shallow_arg, &
+        & core_mass_arg, core_radius_arg
+    
+    real(dp) :: Q_heating_shallow, core_mass, core_radius
     type(NScool_info), pointer :: s=>null()
     integer :: ierr, NScool_id, i
     
@@ -31,43 +33,65 @@ program run_dStar
         & 'Qheating','shallow heating per nucleon',ierr, &
         & flag='Q',takes_parameter=.TRUE.)
     call check_okay('set command argument shallow heating',ierr)
+
+    call command_arg_set( &
+        & 'Mcore','core mass',ierr, &
+        & flag='M',takes_parameter=.TRUE.)
+    call check_okay('set command argument core mass',ierr)
+
+    call command_arg_set( &
+        & 'Rcore','core radius',ierr, &
+        & flag='R',takes_parameter=.TRUE.)
+    call check_okay('set command argument core radius',ierr)
     
     call parse_arguments(ierr)
     call check_okay('parse_arguments',ierr)
 
     my_dStar_dir = trim(get_command_arg('dStar_directory'))
     if (len_trim(my_dStar_dir)==0) my_dStar_dir = default_dStar_dir
+
     inlist = trim(get_command_arg('inlist_file'))
     if (len_trim(inlist)==0) inlist = default_inlist_file
+
     Q_heating_shallow_arg = trim(get_command_arg('Qheating'))
     if (len_trim(Q_heating_shallow_arg)==0)  &
         & Q_heating_shallow_arg = default_Q_heating_shallow
     read(Q_heating_shallow_arg,*) Q_heating_shallow
+
+    core_mass_arg = trim(get_command_arg('Mcore'))
+    if (len_trim(core_mass_arg)==0)  &
+        & core_mass_arg = default_core_mass
+    read(core_mass_arg,*) core_mass
+    print *, 'core mass = ',core_mass
+
+    core_radius_arg = trim(get_command_arg('Rcore'))
+    if (len_trim(core_radius_arg)==0)  &
+        & core_radius_arg = default_core_radius
+    read(core_radius_arg,*) core_radius
     
     call NScool_init(my_dStar_dir, ierr)
     call check_okay('NScool_init',ierr)
     
     NScool_id = alloc_NScool(ierr)
     call check_okay('NScool_id',ierr)
-    
+
     call NScool_setup(NScool_id,inlist,ierr)
     call check_okay('NScool_setup',ierr)
     
-    call NScool_create_model(NScool_id,ierr)
-    call check_okay('NScool_create_model',ierr)
-
     call get_NScool_info_ptr(NScool_id,s,ierr)
     call check_okay('get_NScool_info_ptr',ierr)
-    s% Q_heating_shallow = Q_heating_shallow 
-!     write(output_unit,'(a,f4.1,a)') 'Q shallow = ',s% Q_heating_shallow,' MeV'
-
+    s% Q_heating_shallow = Q_heating_shallow
+    s% core_mass = core_mass
+    s$ core_radius = core_radius
+    s% Mcore = core_mass
+    s% Rcore = core_radius
+    
+    call NScool_create_model(NScool_id,ierr)
+    call check_okay('NScool_create_model',ierr)
+    
     call NScool_evolve_model(NScool_id,ierr)        
     call check_okay('NScool_evolve_model',ierr)
 
-!     call get_NScool_info_ptr(NScool_id,s,ierr)
-!     call check_okay('get_NScool_info_ptr',ierr)
-    
-    
     write(output_unit,*)
     write(output_unit,'(a7,a6,a11)') 'time','Teff','Flux/mdot'
     write(output_unit,'(a7,a6,a11)') '[d]','[MK]','[MeV/u]'
